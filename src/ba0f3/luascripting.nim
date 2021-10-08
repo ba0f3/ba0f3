ce833fbd5b092382fd743f530c3fa825160db4b9
Huy Doan <me@huy.im>
2021-10-09 01:42:28 +0700

    add no cache version of `executeScript`

--- src/ba0f3/luascripting.nim
+++ src/ba0f3/luascripting.nim
@@ -14,11 +14,15 @@ proc getScript*(L: LuaState, path: string): bool =
     var ret = L.loadfile(path)
     debug "GetScript", path, ret
     if ret != LUA_OK:
+      when not defined(release):
+        echo L.error()
       error "Load script error", ret
       return false
 
     ret = L.pcall(0, LUA_MULTRET, 0)
     if ret != LUA_OK:
+      when not defined(release):
+        echo L.error()
       error "Script error", ret
       return false
     L.pushvalue(-1)
@@ -29,8 +33,18 @@ proc getScript*(L: LuaState, path: string): bool =
   return true
 
 template executeScript*(L: LuaState, path: string, fn: string, args: varargs[untyped]) =
-  debug "ExecuteScript", path=path, fun=fn
+  debug "ExecuteScript", script=path, fun=fn
   if L.getScript(path):
+    L.getglobal(fn)
+    let top = L.gettop()
+    if L.luatype(top) == LUA_TFUNCTION:
+      callfunctionImpl("L", args)
+    else:
+      warn "Invalid function call", fun=fn
+
+template executeScriptNoCache*(L: LuaState, path: string, fn: string, args: varargs[untyped]) =
+  debug "ExecuteScript", script=path, fun=fn
+  if L.dofile(path) == LUA_OK:
     L.getglobal(fn)
     let top = L.gettop()
     if L.luatype(top) == LUA_TFUNCTION:
